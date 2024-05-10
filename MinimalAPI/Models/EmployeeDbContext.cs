5bcbbeaa227dbe50c08e148544cc038df95c7b03
AkshayPhadke <akshayaphadke@gmail.com>
2024-05-10 16:35:55 +0530

    Add project files.

---
+++ MinimalAPI/Models/EmployeeDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace MinimalAPI.Models
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ MinimalAPI/Models/LoginVM.cs
@@ -0,0 +1,8 @@
+﻿namespace MinimalAPI.Models
+{
+    public class LoginVM
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
